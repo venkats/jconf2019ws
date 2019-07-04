e024d66c2b424a9976e835a6b283518483eb54f4
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-04 15:06:25 -0500

    behavior of subscribeOn

---
+++ examples/part7/03_behavior_of_subscribeOn/com/agiledeveloper/Sample.java
@@ -0,0 +1,41 @@
+package com.agiledeveloper;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.schedulers.Schedulers;
+
+public class Sample {
+  public static void main(String[] args) throws InterruptedException {
+    Flowable.<Integer>create(emitter -> emit(emitter), BackpressureStrategy.BUFFER)
+      .filter(data -> check("1", data))
+      .subscribeOn(Schedulers.computation())
+      .filter(data -> check("2", data))
+      .subscribe(data -> print(data));
+
+    //subscribeOn will ensure the entire pipeline runs on the
+    //same thread (different from main though).
+
+    System.out.println("DONE");
+
+    Thread.sleep(10000);
+  }
+
+  private static boolean check(String msg, Integer data) {
+    System.out.println("check " + msg + " " + Thread.currentThread());
+    return true;
+  }
+
+  private static void print(Integer data) {
+    System.out.println("received " + data + " in " + Thread.currentThread());
+  }
+
+  private static void emit(FlowableEmitter<Integer> emitter) {
+    int count = 0;
+
+    while(count++ < 10) {
+      System.out.println("emitting from " + Thread.currentThread());
+      emitter.onNext(count);
+    }
+  }
+}
