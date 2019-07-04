4eb193e89aa01effdd953b63a9e6d116faa3e98a
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-04 11:03:56 -0500

    post lab

---
+++ labs/part3/Sample.java
@@ -0,0 +1,8 @@
+public class Sample {
+  public static void main(String[] args) throws InterruptedException {
+    final List<String> symbols = List.of(
+        "AMD", "HPQ", "IBM", "TXN", "VMW", "XRX", "AAPL", "ADBE",
+        "AMZN", "CRAY", "CSCO", "SNE", "GOOG", "INTC", "INTU", "MSFT", "ORCL", "TIBX", "VRSN", "YHOO");
+
+  }
+}
---
+++ labs/part3/StockPrice.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class StockPrice {
+  public static int getPrice(final String ticker) {
+    //This simply create some fake prices
+    return new Random().nextInt(2000);
+  }
+}
