cba38b44c42eca7fd1c215ea9573d182d16b4d28
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-04 10:41:26 -0500

    being reactive

---
+++ examples/part2/01_being_reactive/com/agiledeveloper/Sample.java
@@ -0,0 +1,31 @@
+package com.agiledeveloper;
+
+public class Sample {
+  public static void main(String[] args) {
+    /*
+      Reactive is a next logical step from functional programming
+
+      functional programming is using functional composition and
+        lazy evaluation
+
+      Reactive programming also uses functional composition and
+        lazy evaluation, and builds on top of those ideas
+
+      We take the functional programming ideas and work on
+      different channels for communication along with a better/different
+      way to deal with error handling.
+
+      exceptions are imperative style of programming idea
+
+      Functional is mutually exclusive with exceptions
+
+      Reactive deals with exceptions down stream, built as a programming
+      model
+
+
+      It separate dealing with data from handling error nicely.
+
+      It is based on the idea of circuit breaker
+     */
+  }
+}
