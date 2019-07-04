e0f990a4c1da602686f752e995001a296d937d92
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-07-04 14:20:24 -0500

    looking deeper into cold flowable

---
+++ examples/part6/03_looking_deeper_into_cold_flowable/com/agiledeveloper/Sample.java
@@ -0,0 +1,46 @@
+package com.agiledeveloper;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.schedulers.Schedulers;
+
+public class Sample {
+  public static void print(String msg, long data) {
+    System.out.println(msg + ":" + data);
+  }
+  public static void main(String[] args) throws InterruptedException {
+    Flowable<Long> flowable = //Flowable.interval(0, 1, TimeUnit.SECONDS);
+        Flowable.<Long>create(emitter -> emit(emitter), BackpressureStrategy.BUFFER)
+        .subscribeOn(Schedulers.computation());
+
+    //by default the flowables are cold
+
+    flowable.subscribe(data -> print("S1", data));
+
+    Thread.sleep(5000);
+
+    flowable.subscribe(data -> print("S2", data));
+
+    Thread.sleep(10000);
+  }
+
+  private static void emit(FlowableEmitter<Long> emitter) {
+    System.out.println("Got a new subscription");
+
+    long count = 0;
+    while(count++ < 10) {
+      emitter.onNext(count);
+      sleep(1000);
+    }
+  }
+
+  private static boolean sleep(int ms) {
+    try {
+      Thread.sleep(ms);
+      return true;
+    } catch (InterruptedException e) {
+      return false;
+    }
+  }
+}
